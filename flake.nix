201f33f406ea3ffd514fe198beb71a30011f43e8
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-13 03:27:14 +0900

    feat: update dev environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "dotenv-caster";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+          ];
+        };
+      }
+    );
+}
