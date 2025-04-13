1b9434c801b22c0a96ffd00e1bb277092b7a0283
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-13 17:31:07 +0900

    feat: add test config

---
+++ vitest.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    // @vitest/coverage-v8
+    coverage: {
+      enabled: true,
+      reporter: ['html'],
+      exclude: [
+        '.direnv/**/*',
+        '.github/**/*',
+        '.vscode/**/*',
+        'coverage/**/*',
+        'dist/**/*',
+        'html/**/*',
+        'docs/**/*',
+        'node_modules/**/*',
+        '.envrc',
+        '.gitignore',
+        '.npmignore',
+        '.prettierrc',
+        'flake.lock',
+        'flake.nix',
+        'LICENSE',
+        'package-lock.json',
+        'package.json',
+        'README.md',
+        'renovate.json',
+        'tsconfig.json',
+        'typedoc.json',
+        'vite.config.ts',
+      ],
+    },
+    // @vitest/ui
+    reporters: ['default', 'html'],
+  },
+});
